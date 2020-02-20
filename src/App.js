e2b2b5b9b5d000615f1a904dd946f728b091146c
daniel mcnamara <danmac8407@gmail.com>
2020-02-19 18:19:05 -0600

    issues pushing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import Header from "./Components/Header";
 import Container from "./Components/Container";
 
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> ce16132930a5195bfe67f45dd7db23027b51877f
 function App() {
   return (
     <div className="App">
