3f08d6c09200f6a89c9f613210511d6161c9a5fa
daniel mcnamara <danmac8407@gmail.com>
2020-02-20 18:10:19 -0600

    changed a color

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,14 +4,14 @@ import styled from 'styled-components';
 const HeaderDiv = styled.div`
     box-sizing: border-box;
     padding: 50px 5%;
-    background: dimgray;
+    background: teal;
     margin: 0;
 `
 
 export default function Header() {
     return (
         <HeaderDiv>
-            <h1>NASA Astronomy Picture of the Day</h1>
+            <h1>NASA Image of the Day</h1>
         </HeaderDiv>
     );
 }
